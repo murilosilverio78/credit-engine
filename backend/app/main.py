b8ab1616795223511a2e2a1a22433c84511714f5
Murilo Silverio <murilo@antecipagov.com.br>
2026-05-26 16:14:05 -0300

    fix: allow localhost:3000 in CORS origins

--- backend/app/main.py
+++ backend/app/main.py
@@ -12,7 +12,13 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"] if settings.DEBUG else [settings.FRONTEND_URL],
+    allow_origins=["*"]
+    if settings.DEBUG
+    else [
+        settings.FRONTEND_URL,
+        "http://localhost:3000",
+        "http://127.0.0.1:3000",
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
